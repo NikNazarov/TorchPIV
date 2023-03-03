535e726efb71e39af93dec388015a43b49eda73c
NikNazarov <nazarov.nik.an@gmail.com>
2023-03-03 18:21:20 +0700

    setup.py removed

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules = cythonize("fastSubpixel.pyx", annotate=True,  compiler_directives={'language_level': "3"}),
-    include_dirs=[np.get_include()]
-)
