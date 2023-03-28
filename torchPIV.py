d25918bb6da6cff1c083bddaa12d9e7d42157aa8
NikNazarov <nazarov.nik.an@gmail.com>
2023-03-28 13:22:50 +0700

    Natural keys added to torchPIV module

--- torchPIV.py
+++ torchPIV.py
@@ -7,7 +7,15 @@ from torch.utils.data import Dataset
 from scipy import interpolate
 import cv2
 from time import time
-from PlotterFunctions import natural_keys
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
 class DeviceMap:
     devicies = {
